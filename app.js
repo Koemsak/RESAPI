5f5c7da8e20368b284adb6f4351ebc5e62fc7bf6
koemsak.mean <koemsak.mean@wingmoney.com>
2023-11-10 09:08:51 +0700

    Add console log

--- app.js
+++ app.js
@@ -74,8 +74,9 @@ app.delete("/api/users/:id", (req, res) => {
 })
 
 app.post("/api/callback", (req, res) => {
-    const inputField = JSON.parse(request.body);
-    console.log(`Request data from partner: ${request.body}`);
+    console.log(req.body);
+    const inputField = JSON.parse(req.body);
+    console.log(`Request data from partner: ${req.body}`);
     const contents = inputField.contents;
     const password = "277a44d5ca16cd81ae9538f1269182df";
     const iv = Buffer.from(password.substring(0, 16), 'utf-8');
